fa3b627d19c6f003084df3e325a58cf801b41833
Shuanglong Liu <shlliu@physicsd2348a.local>
2026-03-02 14:47:11 -0500

    -

--- tools/tool_RK4.py
+++ tools/tool_RK4.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
 
     # Set up the quantum master equation
     lio = liouville(eff, dynamics)
-    # lio.get_initial_rho(from_file=False)
+    lio.get_initial_rho(from_file=False)
     # lio.get_initial_rho(from_file=True, 
     #      fname="./output/T_0.6K_I0_1.00e-14_lambdaa_10.00/Bt_linear_sweep_rate_5.0e-08/rho_RK4/0.000-0.100ps_dt0.001ps.h5",
     #      t_init=lio.tmin)
-    # lio.evolve_rho(method="RK4")
+    lio.evolve_rho(method="RK4")
